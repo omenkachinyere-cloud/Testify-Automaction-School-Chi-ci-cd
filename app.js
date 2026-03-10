05785d7c776b363ac375d20a07dc8d71315ee4ae
Chiqa <omenkachinyere@gmail.com>
2026-03-10 12:12:46 +0100

    task6.js

--- app.js
+++ app.js
@@ -2,7 +2,20 @@
 const Subject = 'JavaMyth'
 console.log(Subject);
 
+//Task 3
 const dayOfTheWeek = 'Tuesday';
 const position = 3;
 const output = 'Today is ' + dayOfTheWeek  +'.'  + ' It is the ' + position + 'rd' + ' day of the week.';
 console.log(output);
+
+const firstName = 'John';
+const lastName = 'Friend';
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+const age = 16;
+if (age >= 18) {
+    console.log('You are an adult and eligible to vote.');
+} else {
+    console.log('You are not an adult and not eligible to vote.');
+}
